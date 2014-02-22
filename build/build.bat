61392cbf1cd1139b2170d5623e5a0440613f3aad
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-02-22 10:23:25 +0100

    Add release build command.

--- build/build.bat
+++ build/build.bat
@@ -9,4 +9,7 @@ cd %this_path%\..\src
 set output_path="%bin_path%\wkBindKeys.dll"
 rem set output_path="C:\Worms Armageddon\wkBindKeys.dll"
 
-rdmd --build-only -shared -w -g -I%home_path% -I%home_path%\src -od%bin_path% -of%output_path% -L/map -version=WindowsXP wkBindKeys\wkBindKeys.def wkBindKeys\main.d
+set "build_flags=-w -g"
+rem set "build_flags=-release -inline -O -noboundscheck"
+
+rdmd --build-only -shared %build_flags% -I%home_path% -I%home_path%\src -od%bin_path% -of%output_path% -L/map -version=WindowsXP wkBindKeys\wkBindKeys.def wkBindKeys\main.d
